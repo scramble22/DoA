c9f01cbd8eae65176247b21d48ea4727fd28b26f
Scramble22 <Scramble22@users.noreply.github.com>
2023-12-29 19:39:07 +0300

    v1.1.9
    male animations and fix bugs
    make new abbreviations

--- main.js
+++ main.js
@@ -20,9 +20,21 @@ document.addEventListener('DOMContentLoaded', function() {
         { term: 'TL;DR', definition: 'Too Long; Didn\'t Read', rus: 'Слишком длинно, не читал' },
         { term: 'ETA', definition: 'Estimated Time of Arrival', rus: 'Ожидаемое время прибытия' },
         { term: 'FAQ', definition: 'Frequently Asked Questions', rus: 'Часто задаваемые вопросы' },
-        { term: 'VIP', definition: 'Very Important Person', rus: 'Очень важная личность' }
+        { term: 'VIP', definition: 'Very Important Person', rus: 'Очень важная личность' },
+        { term: 'LMAO', definition: 'laughing my ass off', rus: 'дико смеяться' },
+        { term: 'WTF ', definition: 'What The F***', rus: 'что за..? ' },
+        { term: 'BB', definition: 'Bye-bye', rus: 'Пока!' },
+        { term: 'BBS', definition: 'be back soon', rus: 'Cкоро буду, скоро вернусь' },
+        { term: 'Bgg', definition: 'Bu-ga-ga', rus: 'сокращение от Бу-га-га, что значит сильный смех' },
+        { term: 'BRB', definition: 'be right back', rus: 'скоро вернусь (вернусь через минуту)' },
+        { term: 'BSoD', definition: 'Blue Screen of Death', rus: 'синий экран смерти. Экран с сообщением о невосстановимой ошибке ядра, после которой можно только перезагрузить компьютер кнопкой «Reset». В операционных системах семейства Windows он синего цвета.' }
         // Добавь больше сокращений, если нужно
     ];
+
+
+// Так же вы можете посмотреть еще некоторые сокращения: 
+// * Black magic — работающий программный код неясного устройства
+// * BOFH — Bastard Operator From Hell (Адский Оператор-ублюдок) , герой одноименной серии рассказов.
 
     const isRussian = localStorage.getItem('isRussian') === 'true';
     const sortedAbbreviations = abbreviations.sort((a, b) => a.term.localeCompare(b.term));
@@ -130,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Функция для перенаправления на страницу с названием
     function redirectToTermPage(term) {
-        const destinationPage = term.toLowerCase() + '.html';
+        const destinationPage = 'Pages/' + term.toLowerCase() + '.html';
 
         // Проверяем существование файла перед перенаправлением
         checkFileExists(destinationPage)
